9720eea2b16d24af61ef65ec28b0347d8b7971d7
shenjianjun <978622005@qq.com>
2020-03-24 18:00:25 +0800

    success

--- app/src/main/java/com/sjianjun/jitpackio/MainActivity.kt
+++ app/src/main/java/com/sjianjun/jitpackio/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sjianjun.jitpackio
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sjianjun.jitpackio.single.TestLib
+import com.sjianjun.lib2.TestLib2
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +12,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         TestLib.print()
+        TestLib2.print()
     }
 }
