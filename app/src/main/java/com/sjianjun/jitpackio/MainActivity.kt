925a216e643c295445a2017200fcd4234f83427e
shenjianjun <978622005@qq.com>
2020-03-24 17:02:59 +0800

    log

--- app/src/main/java/com/sjianjun/jitpackio/MainActivity.kt
+++ app/src/main/java/com/sjianjun/jitpackio/MainActivity.kt
@@ -2,11 +2,14 @@ package com.sjianjun.jitpackio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sjianjun.jitpackio.single.TestLib
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        TestLib.print()
     }
 }
---
+++ single/src/main/java/com/sjianjun/jitpackio/single/TestLib.java
@@ -0,0 +1,9 @@
+package com.sjianjun.jitpackio.single;
+
+import android.util.Log;
+
+public class TestLib {
+    public static void print() {
+        Log.e("test", "this single lib");
+    }
+}
