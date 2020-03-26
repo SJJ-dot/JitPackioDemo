0e02d169a9e2a779fa1da40a0d899d799976bc09
shenjianjun <978622005@qq.com>
2020-03-26 17:36:26 +0800

    test

--- app/src/main/java/com/sjianjun/jitpackio/MainActivity.kt
+++ app/src/main/java/com/sjianjun/jitpackio/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sjianjun.jitpackio.single.TestLib
 import com.sjianjun.lib2.TestLib2
+import com.sjianjun.retrofit.libjava.MyClass
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +14,6 @@ class MainActivity : AppCompatActivity() {
 
         TestLib.print()
         TestLib2.print()
+        MyClass()
     }
 }
